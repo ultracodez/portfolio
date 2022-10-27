42c30e66b6deca3b5fa2d49dc3064a57608f513c
ultracodez <cuentajugar2018@hotmail.com>
2022-10-26 19:30:07 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -30,6 +30,7 @@ const sentryWebpackPluginOptions = {
 	silent: false, // Suppresses all logs
 	// For all available options, see:
 	// https://github.com/getsentry/sentry-webpack-plugin#options.
+	authToken: process.env.SENTRY_AUTH_TOKEN_SECURE,
 };
 
 module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
